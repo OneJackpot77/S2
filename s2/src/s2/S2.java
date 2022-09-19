539891866ee0e00f4e4ee2b4dd961889029ebc1c
OneJackpot77 <113238347+OneJackpot77@users.noreply.github.com>
2022-09-19 13:32:45 +0200

    Add files via upload

---
+++ s2/src/s2/S2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s2;
+
+/**
+ *
+ * @author user05
+ */
+public class S2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s1 = new String();
+        String s2 = new String();
+      String s3 = new java.lang.String("Ez " + 1 + " sztring!");
+       String s11 = "ASD-123";
+         String s6 = new String("lacika");
+        String[] tomb = {"asd","hgj"};
+         boolean start = "file.java".startsWith("fi");
+        char[] karaktertomb = {'S', 'o', 'p', 'r', 'o', 'n'};
+        boolean vege = "file.java".endsWith(".jav"); 
+       StringBuffer sb1= new StringBuffer();   // L:0  C:16
+        System.out.println(sb1.length()+ " " + sb1.capacity());
+        StringBuffer sb2= new StringBuffer(10); // L:0  C:10
+        System.out.println(sb2.length()+ " " + sb1.capacity());
+        String varos = "Sopron";
+      
+        System.out.print(tomb[1]);
+        
+        // TODO code application logic here
+    }
+    
+}
